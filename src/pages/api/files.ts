128aaefdf2528a89bc0bc70ed3b194249927cd03
jooooonis <xowns3213@naver.com>
2022-11-20 21:20:19 +0900

    feat : api utils

---
+++ src/pages/api/files.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
+};
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const response = await (
+    await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/images/v2/direct_upload`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.CF_TOKEN}`,
+        },
+      },
+    )
+  ).json();
+
+  res.status(200).json({ name: 'John Doe', ...response.result });
+}
+
+export default handler;
