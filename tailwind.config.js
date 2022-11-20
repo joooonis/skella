dcd510e7106fe2c6cdd46e42dd43fcbe56c8f3ac
jooooonis <xowns3213@naver.com>
2022-11-20 21:16:46 +0900

    🚀 feat : init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {},
+  plugins: [require('@tailwindcss/forms')],
+};
