5d547d8bbb3e089e30ab1f67295678ff8d9b5ba2
jooooonis <xowns3213@naver.com>
2022-11-20 21:16:58 +0900

    feat : swr settings

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
+      }}
+    >
+      <div className="mx-auto w-full max-w-xl">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
+  );
+}
