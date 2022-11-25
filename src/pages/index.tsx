8fb6afe332734f60ce90d31889d76724fc7fab86
jooooonis <xowns3213@naver.com>
2022-11-25 23:38:17 +0900

    design : place center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 export default function Home() {
   const { mutate, data, error } = useSWR('api/images');
   return (
-    <div className="flex w-full max-w-lg flex-col items-center space-y-4 p-6">
-      <div className="mb-2 bg-black">
-        <img className="w-full" src="/title.png" />
+    <div className="itmes-center flex w-full  flex-col space-y-4 p-6 tablet:space-y-8 tablet:p-24">
+      <div className="mb-2 flex items-center justify-center bg-black">
+        <img className="w-full tablet:w-1/2" src="/title.png" />
       </div>
       {data?.images?.map((img: any) => (
-        <div className="w-full">
+        <div className="flex w-full items-center justify-center">
           <img
             src={`https://imagedelivery.net/M9q4wVMn9ZCCxsqKW1CbSw/${img.img}/public`}
           />
