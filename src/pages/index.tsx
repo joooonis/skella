570223728161c285aae293609d1ee73e668c234c
jooooonis <xowns3213@naver.com>
2022-12-01 14:19:08 +0900

    design : responsive mobile - default tablet - 820px

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,19 @@ import Image from 'next/image';
 export default function Home() {
   const { mutate, data, error } = useSWR('api/images');
   return (
-    <div className="itmes-center flex w-full  flex-col space-y-4 p-6 tablet:space-y-8 tablet:p-24">
-      <div className="mb-2 flex items-center justify-center bg-black">
-        <img className="w-full tablet:w-1/2" src="/title.png" />
+    <div className="itmes-center flex w-full  flex-col p-6  tablet:p-24">
+      <div className="mb-2 flex items-center justify-center bg-black tablet:mb-16">
+        <img className="w-full tablet:w-80" src="/title.png" />
       </div>
-      {data?.images?.map((img: any) => (
-        <div className="flex w-full items-center justify-center">
-          <img
-            src={`https://imagedelivery.net/M9q4wVMn9ZCCxsqKW1CbSw/${img.img}/public`}
-          />
-        </div>
-      ))}
+      <div className="space-y-4 tablet:space-y-8">
+        {data?.images?.map((img: any) => (
+          <div className="mx-auto flex w-full max-w-2xl items-center justify-center ">
+            <img
+              src={`https://imagedelivery.net/M9q4wVMn9ZCCxsqKW1CbSw/${img.img}/public`}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
